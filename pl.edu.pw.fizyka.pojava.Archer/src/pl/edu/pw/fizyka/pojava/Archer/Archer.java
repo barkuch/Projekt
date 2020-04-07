0919486150087953ef0396db59e4524388e552af
barkuch <63244215+barkuch@users.noreply.github.com>
2020-04-07 17:22:57 +0200

    cos

--- pl.edu.pw.fizyka.pojava.Archer/src/pl/edu/pw/fizyka/pojava/Archer/Archer.java
+++ pl.edu.pw.fizyka.pojava.Archer/src/pl/edu/pw/fizyka/pojava/Archer/Archer.java
@@ -138,10 +138,10 @@ public class Archer extends JFrame
 	        
 	        //panel z animacj¹
 	        animationPanel = new JPanel();
-	        animationPanel.setBounds(1, 1, 1180, 400); //ustawiam rozmiar panelu
+	        animationPanel.setBounds(1, 1, 1180, 400); //ustawiam rozmiar panelu	        	        
 	        animationPanel.setBackground(Color.white); //ustawiam kolor panelu
 	        add(animationPanel);
-	        this.add(animationPanel, BorderLayout.CENTER); //wykorzystanie BorderLayoutu
+	       // this.add(animationPanel, BorderLayout.CENTER); //wykorzystanie BorderLayoutu
 	        
 	            
 	        //panel ustawieñ
